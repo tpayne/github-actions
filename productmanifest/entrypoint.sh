845ff37bc42a0904dfa87c16d8e5fa8f13be98d8
Tim Payne <tpayne@users.noreply.github.com>
2022-04-16 02:06:57 +0100

    Update entrypoint.sh

--- productmanifest/entrypoint.sh
+++ productmanifest/entrypoint.sh
@@ -142,7 +142,7 @@ testDocker()
 {
 echo "${command}: Test login..."
 rmFile "${tmpFile}"
-(docker login https://index.${1}/v1/ -u ${2} -p ${3}) > "${tmpFile}" 2>&1
+(docker login ${1}/ -u ${2} -p ${3}) > "${tmpFile}" 2>&1
 if [ $? -gt 0 ]; then
     cat "${tmpFile}"
     rmFile "${tmpFile}"
